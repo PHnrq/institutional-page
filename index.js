c7b60976d8be59414d8dd52129f3d4fb0ab57336
PHnrq <paulofigueredo7@gmail.com>
2022-03-14 10:45:04 -0300

    add interactive menu

---
+++ index.js
@@ -0,0 +1,21 @@
+function showMenu(){
+    let nav = document.getElementById('nav');
+    let showMenuImg = document.getElementById('showMenuImg')
+    let logo = document.getElementById('logo')
+    let logoMenuBtn = document.getElementById('logoMenuBtn')
+
+    if(nav.style.display === 'none'){
+        nav.style.display = 'flex'
+        showMenuImg.classList.remove('showMenuImg')
+        showMenuImg.classList.add('closeMenuImg')
+        logo.style.display = 'none'
+        logoMenuBtn.style.flexDirection = 'row-reverse'
+
+    } else{
+        nav.style.display = 'none'
+        showMenuImg.classList.remove('closeMenuImg')
+        showMenuImg.classList.add('showMenuImg')
+        logo.style.display = 'flex'
+        logoMenuBtn.style.flexDirection = 'row'
+    }
+}
