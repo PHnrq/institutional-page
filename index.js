488165170a513f58e2b9be95632ac358d83866f4
PHnrq <paulofigueredo7@gmail.com>
2022-03-17 11:11:34 -0300

    apply swiper testimonials

--- index.js
+++ index.js
@@ -19,3 +19,11 @@ function showMenu(){
         logoMenuBtn.style.flexDirection = 'row'
     }
 }
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
