3e82bf729051b74302c622550620cb26279e588b
PHnrq <paulofigueredo7@gmail.com>
2022-03-30 09:53:10 -0300

    change inline style switch by classe switch

--- index.js
+++ index.js
@@ -4,18 +4,22 @@ function showMenu(){
     let logo = document.getElementById('logo')
     let logoMenuBtn = document.getElementById('logoMenuBtn')
 
-    if(nav.style.display === 'none'){
-        nav.style.display = 'flex'
+    if(nav.classList.contains('nav-none')){
+        nav.classList.remove('nav-none')
+        nav.classList.add('nav-flex')
         showMenuImg.classList.remove('showMenuImg')
         showMenuImg.classList.add('closeMenuImg')
-        logo.style.display = 'none'
+        logo.classList.remove('logo-flex')
+        logo.classList.add('logo-none')
         logoMenuBtn.style.flexDirection = 'row-reverse'
 
     } else{
-        nav.style.display = 'none'
+        nav.classList.remove('nav-flex')
+        nav.classList.add('nav-none')
         showMenuImg.classList.remove('closeMenuImg')
         showMenuImg.classList.add('showMenuImg')
-        logo.style.display = 'flex'
+        logo.classList.remove('logo-none')
+        logo.classList.add('logo-flex')
         logoMenuBtn.style.flexDirection = 'row'
     }
 }
